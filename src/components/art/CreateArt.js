3bacd1c5e8b29e002e085ad00629319041a64764
Tommy Chung <tommychung@TC-MacBook-Pro.local>
2021-08-20 11:00:48 -0400

    Updated

--- src/components/art/CreateArt.js
+++ src/components/art/CreateArt.js
@@ -35,7 +35,7 @@ const CreateArt = (props) => {
                 <label htmlFor="description">Description: </label>
                 <input type="text" name="description" onChange={handleChange} /><br/>
                 <label htmlFor="created_date">Created Date: </label>
-                <input type="text" name="created_date" onChange={handleChange} /><br/>
+                <input type="date" name="created_date" onChange={handleChange} /><br/>
                 <input type='submit' />
             </form>
         </div>
