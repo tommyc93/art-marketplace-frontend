881bfbde38decea718ac0415b5ec75786d820c2e
Danish Virani <danishvirani.tech@gmail.com>
2021-08-20 12:50:44 -0400

    created navbar andpage views

--- src/components/art/EditArt.js
+++ src/components/art/EditArt.js
@@ -19,25 +19,25 @@ const EditArt = (props) => {
 
     ///////////////---------Return---------///////////////
     return (
-        <div>
+        <div class='mb-3 w-25 mx-auto'>
             <details>
             <summary>Edit Art</summary>
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor="title">Title: </label>
-                    <input type="text" name="title" onChange={handleChange} value={piece.title}/><br/>
-                    <label htmlFor="author">Author: </label>
-                    <input type="text" name="author" onChange={handleChange} value={piece.author}/><br/>
-                    <label htmlFor="rating">Rating: </label>
-                    <input type="number" name="rating" onChange={handleChange} value={piece.rating}/><br/>
-                    <label htmlFor="image">Image: </label>
-                    <input type="text" name="image" onChange={handleChange} value={piece.image}/><br/>
-                    <label htmlFor="price">Price: </label>
-                    <input type="number" name="price" onChange={handleChange} value={piece.price}/><br/>
-                    <label htmlFor="description">Description: </label>
-                    <input type="text" name="description" onChange={handleChange} value={piece.description}/><br/>
-                    <label htmlFor="created_date">Created Date: </label>
-                    <input type="date" name="created_date" onChange={handleChange} value={piece.created_date}/><br/>
-                    <input type='submit' />
+                    <label htmlFor="title" class="form-label">Title: </label>
+                    <input type="text" name="title" onChange={handleChange} value={piece.title} class="form-control"/><br/>
+                    <label htmlFor="author" class="form-label">Author: </label>
+                    <input type="text" name="author" onChange={handleChange} value={piece.author} class="form-control"/><br/>
+                    <label htmlFor="rating" class="form-label">Rating: </label>
+                    <input type="number" name="rating" onChange={handleChange} value={piece.rating} class="form-control"/><br/>
+                    <label htmlFor="image" class="form-label">Image: </label>
+                    <input type="text" name="image" onChange={handleChange} value={piece.image} class="form-control"/><br/>
+                    <label htmlFor="price" class="form-label">Price: </label>
+                    <input type="number" name="price" onChange={handleChange} value={piece.price} class="form-control"/><br/>
+                    <label htmlFor="description" class="form-label">Description: </label>
+                    <textarea name="description" onChange={handleChange} class="form-control"></textarea><br/>
+                    <label htmlFor="created_date" class="form-label">Created Date: </label>
+                    <input type="date" name="created_date" onChange={handleChange} value={piece.created_date} class="form-control"/><br/>
+                    <input type='submit' class='btn btn-outline-dark' />
                 </form>
             </details>
         </div>
