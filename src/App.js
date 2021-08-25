133cc9b6d0fb57bface263eef9683fe06a164aae
Oduaine Miller <oduainemiller@Oduaines-Air.fios-router.home>
2021-08-24 20:35:02 -0400

    added carousel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick"
 //====Art====//
 import CreateArt from './components/art/CreateArt'
 import EditArt from './components/art/EditArt'
@@ -30,6 +33,21 @@ const App = () => {
     let [artists, setArtists] =useState([])
 
     ///////////////---------Functions---------///////////////
+
+    //////////////---------Carousel Settings-----/////////////
+
+    const settings = {
+  dots: true,
+  fade: true,
+  infinite: true,
+  speed: 500,
+  slideToShow: 1,
+  className: "slides",
+  autoplay: true,
+  speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+}
     //====Create====//
     const handleCreate = (addArt) => {
         axios
@@ -161,7 +179,24 @@ const App = () => {
             {currentView == 'showArt' &&
                 <>
                 <br/><br/>
+
+                <div class='mx-auto w-50 h-25'>
+
+                <Slider {...settings}>
+                {artCollection.map((pieces) => {
+                  return(
+                    <div>
+                    <img class="s-image" src ={pieces.image}/>
+                    </div>
+                  )
+                })}
+                </Slider>
+                </div>
+                <br/><br/>
                 <div class='d-flex flex-wrap mx-auto text-center'>
+
+
+
                 {artCollection.map((pieces) => {
                     return <ShowArt prop={pieces} />
                 })}
@@ -174,7 +209,7 @@ const App = () => {
                 <div class='d-flex flex-wrap mx-auto'>
                 {artCollection.map((pieces) => {
                   return (
-                      <div class='card flex-even'>
+                      <div class='card flex-even' picf>
                           <ShowArt
                               prop={pieces}
                           />
